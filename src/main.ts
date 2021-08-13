372db2607693cad64776b8202c11537ac3693896
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-08-13 14:21:53 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   
   app.use(cookieParser());
-  app.enableCors({
-    credentials: true,});
+  app.enableCors();
 
   
   
