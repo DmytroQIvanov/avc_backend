463e0e3b3658fc3ce3fef84d4dcc211b3b9dd261
DmytroQIvanov <20IvanovDmitriy03@gmail.com>
2021-08-15 17:45:06 +0300

    make it better

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,19 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.use(cookieParser());
-app.enableCors({
-  origin: 'http://localhost:3001',
-credentials: true,
-
-});
-
-  
-  
-  config.update({
-    region:"eu-central-1",
-    accessKeyId:"AKIATGDPX3MLKKDX6KJH",
-    secretAccessKey:'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'
+  app.enableCors({
+    origin: 'localhost',
+    credentials: true,
   });
-  await app.listen(process.env.PORT ||3000);
+
+  config.update({
+    region: 'eu-central-1',
+    accessKeyId: 'AKIATGDPX3MLKKDX6KJH',
+    secretAccessKey: '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',
+  });
+  await app.listen(process.env.PORT || 3000);
   console.log('Server has been started!');
 }
 bootstrap();
